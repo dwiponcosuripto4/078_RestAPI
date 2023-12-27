d21681ea6402f28164f20c26af55033bcfef08f8
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-27 21:09:04 +0700

    fix FormInputSiswa in InsertScreen.kt

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -12,6 +13,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.consumerestapi.R
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
 
@@ -26,12 +29,12 @@ fun FormInputSiswa(
 ) {
     Column (
         modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ){
         OutlinedTextField(
             value = insertUiEvent.nama,
             onValueChange = {onValueChange(insertUiEvent.copy(nama=it)) },
-            label = { Text(stringResource(R.string.nama)) },
+            label = { Text("Nama") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
@@ -39,15 +42,16 @@ fun FormInputSiswa(
         OutlinedTextField(
             value = insertUiEvent.alamat,
             onValueChange = {onValueChange(insertUiEvent.copy(alamat=it)) },
-            label = { Text(stringResource(R.string.alamat)) },
+            label = { Text("Alamat") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
         OutlinedTextField(
-            value = detailSiswa.telpon,
-            onValueChange = {onValueChange(detailSiswa.copy(telpon=it)) },
-            label = { Text(stringResource(R.string.telpon)) },
+            value = insertUiEvent.nohp,
+            onValueChange = {onValueChange(insertUiEvent.copy(nohp=it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(text = "No HP") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
@@ -55,13 +59,13 @@ fun FormInputSiswa(
 
         if (enabled) {
             Text(
-                text = stringResource(R.string.required_field),
-                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+                text = "Isi semua data",
+                modifier = Modifier.padding(start = 12.dp)
             )
         }
         Divider(
-            thickness = dimensionResource(R.dimen.padding_small),
-            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
         )
     }
 }
--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewmodel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewmodel.kt
@@ -1,23 +1,65 @@
 package com.example.consumerestapi.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.repository.KontakRepository
+import kotlinx.coroutines.launch
 
+class InserViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
 
-data class InsertUiEvent(
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
+data class  InsertUiEvent(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val nohp : String = "",
+    val nohp: String = "",
 )
 
-fun InsertUiEvent.toKontak() : Kontak = Kontak(
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
     id = id,
     nama = nama,
-    alamat = alamat,
+    email = alamat,
+    nohp = nohp,
+
+    )
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    alamat = email,
     nohp = nohp,
 )
 
-data class InsertUiEvent(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent()
-)
+
+
 
