c1a26a5628cfa366b0768a95bb8115dd9675dde2
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-27 21:59:21 +0700

    add fun HomeScreen and the parameter

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -41,6 +42,16 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = "Kontak"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
 @Composable
 fun HomeStatus(
     kontakUiState: KontakUIState,
