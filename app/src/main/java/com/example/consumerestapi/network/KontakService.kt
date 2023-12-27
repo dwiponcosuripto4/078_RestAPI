c5aec467337c4266e84958246c6cba27fab2e7b4
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-12-27 22:29:44 +0700

    add suspend fun delete to delete

--- app/src/main/java/com/example/consumerestapi/network/KontakService.kt
+++ app/src/main/java/com/example/consumerestapi/network/KontakService.kt
@@ -1,10 +1,13 @@
 package com.example.consumerestapi.network
 
 import com.example.consumerestapi.model.Kontak
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface KontakService {
     @Headers(
@@ -15,4 +18,7 @@ interface KontakService {
 
     @POST("/kontak")
     suspend fun insertKontak(@Body kontak: Kontak)
+
+    @DELETE("/kontak/{id}")
+    suspend fun deleteKontak(@Path("id") id: Int): Response<Void>
 }
--- app/src/main/java/com/example/consumerestapi/repository/KontakRepository.kt
+++ app/src/main/java/com/example/consumerestapi/repository/KontakRepository.kt
@@ -2,6 +2,7 @@ package com.example.consumerestapi.repository
 
 import com.example.consumerestapi.model.Kontak
 import com.example.consumerestapi.network.KontakService
+import java.io.IOException
 
 interface KontakRepository {
     /** Fetches list of kontak from kontakApi */
@@ -19,5 +20,21 @@ class NetworkKontakRepository(
     // insert kontak
     override suspend fun insertKontak(kontak: Kontak) {
         kontakApiService.insertKontak(kontak)
+    }
+
+    override suspend fun deleteKontak(id: Int) {
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful) {
+                // Handle unsuccessful response (non-2xx status code)
+                throw IOException("Failed to delete Kontak. HTTP status code: ${response.code()}")
+            }
+            else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
